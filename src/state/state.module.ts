bf001b9f3433eb90f94a5a1235d5b12ac5280714
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-14 16:32:37 -0300

    :sparkles: feat: create state

---
+++ src/state/state.module.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { StateController } from './state.controller';
+import { StateService } from './state.service';
+import { StateEntity } from './entities/state.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([StateEntity])],
+  controllers: [StateController],
+  providers: [StateService],
+})
+export class StateModule {}
