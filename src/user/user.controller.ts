8c691ad7ee69571dd57c54a9967886423f7323f2
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-15 16:12:57 -0300

    :sparkles: feat: adding validation pipe and dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { UserEntity } from './entities/user.entity';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
+import { ReturnUserDto } from './dto/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUser(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUser()).map(
+      userEntity => new ReturnUserDto(userEntity),
+    );
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,30 @@
-import { CreateUserDto } from './dto/createUser.dto';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { hash } from 'bcrypt';
+import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
   async createUser(createUser: CreateUserDto): Promise<UserEntity> {
-    return null;
+    const saltOrRounds = 10;
+
+    const passwordHashed = await hash(createUser.password, saltOrRounds);
+
+    return this.userRepository.save({
+      ...createUser,
+      typeUser: 1,
+      password: passwordHashed,
+    });
+  }
+
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
