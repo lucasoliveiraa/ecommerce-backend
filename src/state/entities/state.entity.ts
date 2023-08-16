d3c17234bfaf053c671eb897a10d05d3dbe6d4a6
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-16 10:06:50 -0300

    :sparkles: feat: relations and dto state

--- src/state/entities/state.entity.ts
+++ src/state/entities/state.entity.ts
@@ -25,6 +25,6 @@ export class StateEntity {
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 
-  // @OneToMany(() => CityEntity, city => city.state)
-  // cities?: CityEntity[];
+  @OneToMany(() => CityEntity, city => city.state)
+  cities?: CityEntity[];
 }
