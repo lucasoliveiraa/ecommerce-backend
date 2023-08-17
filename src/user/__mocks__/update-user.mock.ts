3421e1cc156ba85ccbf6a417ec2831cc17abc794
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-17 15:56:44 -0300

    :heavy_check_mark: feat: test unit user

---
+++ src/user/__mocks__/update-user.mock.ts
@@ -0,0 +1,11 @@
+// import { UpdatePasswordDTO } from '../dto/update-password.dto';
+
+// export const updatePasswordMock: UpdatePasswordDTO = {
+//   lastPassword: 'abc',
+//   newPassword: 'fdsafj',
+// };
+
+// export const updatePasswordInvalidMock: UpdatePasswordDTO = {
+//   lastPassword: 'lkfdjsa',
+//   newPassword: 'flkjbla',
+// };
