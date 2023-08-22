b5fbcb5fea919cc8cc9646ba88abeb2edcbb0ce0
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-22 14:39:35 -0300

    :sparkles: feat: create product

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,8 @@ import { CacheModule } from './cache/cache.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './guards/roles.guard';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -37,6 +39,8 @@ import { RolesGuard } from './guards/roles.guard';
     CacheModule,
     AuthModule,
     JwtModule,
+    CategoryModule,
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [
