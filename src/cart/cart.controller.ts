f67ca0dc0c7f55c3c284d44f7f331c67e75547c2
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-24 17:10:25 -0300

    ✨ feat: create controller clear and update cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Res,
+  Param,
+  Patch,
+} from '@nestjs/common';
 import { InsertCart } from './dto/insert-cart.dto';
-import { Roles } from 'src/decorators/roles.decorator';
-import { UserType } from 'src/user/enum/user-type.enum';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
 import { CartService } from './cart.service';
-import { UserId } from 'src/decorators/user-id.decorator';
+import { UserId } from '../decorators/user-id.decorator';
 import { ReturnCart } from './dto/return-cart.dto';
+import { DeleteResult } from 'typeorm';
+import { Response } from 'express';
+import { UpdateCart } from './dto/update-cart.dto';
 
 @Roles(UserType.User, UserType.Admin, UserType.Root)
 @Controller('cart')
@@ -18,6 +30,47 @@ export class CartController {
   ): Promise<ReturnCart> {
     return new ReturnCart(
       await this.cartService.insertProductInCart(insertCart, userId),
+    );
+  }
+
+  @Get()
+  async findCartByUserId(
+    @UserId() userId: number,
+    @Res({ passthrough: true }) res?: Response,
+  ): Promise<ReturnCart> {
+    const cart = await this.cartService
+      .findCartByUserId(userId, true)
+      .catch(() => undefined);
+
+    if (cart) {
+      return cart;
+    }
+
+    res.status(204).send();
+
+    return;
+  }
+
+  @Delete()
+  async clearCart(@UserId() userId: number): Promise<DeleteResult> {
+    return this.cartService.clearCart(userId);
+  }
+
+  @Delete('/product/:productId')
+  async deleteProductCart(
+    @Param('productId') productId: number,
+    @UserId() userId: number,
+  ): Promise<DeleteResult> {
+    return this.cartService.deleteProductCart(productId, userId);
+  }
+
+  @Patch()
+  async updateProductInCart(
+    @Body() updateCart: UpdateCart,
+    @UserId() userId: number,
+  ): Promise<ReturnCart> {
+    return new ReturnCart(
+      await this.cartService.updateProductInCart(updateCart, userId),
     );
   }
 }
