f3a37ceb562e539dc42637faf4490b901291aef3
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-24 17:02:03 -0300

    ✔️ feat: create test unit controller address

--- src/address/__mocks__/create-address.mock.ts
+++ src/address/__mocks__/create-address.mock.ts
@@ -1,8 +1,8 @@
 import { cityMock } from '../../city/__mocks__/city.mock';
-import { CreateAddressDto } from '../dto/createAddress.dto';
+import { CreateAddress } from '../dto/createAddress.dto';
 import { addressMock } from './address.mock';
 
-export const createAddressMock: CreateAddressDto = {
+export const createAddressMock: CreateAddress = {
   cep: addressMock.cep,
   cityId: cityMock.id,
   complement: addressMock.complement,
