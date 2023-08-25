4ba2d53a5666a98ef29dbc75767fb32bc8c33a6f
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-25 16:40:16 -0300

    ✨ feat: create relations order-product

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -10,7 +10,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { CartProductEntity } from '../../cart-product/entities/cart-product.entity';
-// import { OrderProductEntity } from '../../order-product/entities/order-product.entity';
+import { OrderProductEntity } from '../../order-product/entities/order-product.entity';
 
 @Entity({ name: 'product' })
 export class ProductEntity {
@@ -60,6 +60,6 @@ export class ProductEntity {
   @JoinColumn({ name: 'category_id', referencedColumnName: 'id' })
   category?: CategoryEntity;
 
-  // @OneToMany(() => OrderProductEntity, orderProduct => orderProduct.product)
-  // ordersProduct?: OrderProductEntity[];
+  @OneToMany(() => OrderProductEntity, orderProduct => orderProduct.product)
+  ordersProduct?: OrderProductEntity[];
 }
