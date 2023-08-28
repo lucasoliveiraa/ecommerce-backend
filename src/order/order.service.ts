e29ccc68dc813c95a08821e4e3c7769ce4b53ef8
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-28 15:44:43 -0300

    ✨ feat: finish service order

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { Repository } from 'typeorm';
-import { PaymentService } from 'src/payment/payment.service';
-import { CartService } from 'src/cart/cart.service';
-import { OrderProductService } from 'src/order-product/order-product.service';
-import { ProductService } from 'src/product/product.service';
+import { PaymentService } from '../payment/payment.service';
+import { CartService } from '../cart/cart.service';
+import { OrderProductService } from '../order-product/order-product.service';
+import { ProductService } from '../product/product.service';
 import { CreateOrderDTO } from './dto/create-order.dto';
-import { PaymentEntity } from 'src/payment/entities/payment.entity';
+import { PaymentEntity } from '../payment/entities/payment.entity';
+import { CartEntity } from '../cart/entities/cart.entity';
+import { ProductEntity } from '../product/entities/product.entity';
+import { OrderProductEntity } from '../order-product/entities/order-product.entity';
 
 @Injectable()
 export class OrderService {
@@ -20,10 +23,120 @@ export class OrderService {
     private readonly productService: ProductService,
   ) {}
 
+  async saveOrder(
+    createOrderDTO: CreateOrderDTO,
+    userId: number,
+    payment: PaymentEntity,
+  ): Promise<OrderEntity> {
+    return this.orderRepository.save({
+      addressId: createOrderDTO.addressId,
+      date: new Date(),
+      paymentId: payment.id,
+      userId,
+    });
+  }
+
+  async createOrderProductUsingCart(
+    cart: CartEntity,
+    orderId: number,
+    products: ProductEntity[],
+  ): Promise<OrderProductEntity[]> {
+    return Promise.all(
+      cart.cartProduct?.map(cartProduct =>
+        this.orderProductService.createOrderProduct(
+          cartProduct.productId,
+          orderId,
+          products.find(product => product.id === cartProduct.productId)
+            ?.price || 0,
+          cartProduct.amount,
+        ),
+      ),
+    );
+  }
+
   async createOrder(
     createOrderDTO: CreateOrderDTO,
     userId: number,
   ): Promise<OrderEntity> {
-    return null;
+    const cart = await this.cartService.findCartByUserId(userId, true);
+    const products = await this.productService.findAll(
+      cart.cartProduct?.map(cartProduct => cartProduct.productId),
+    );
+
+    const payment: PaymentEntity = await this.paymentService.createPayment(
+      createOrderDTO,
+      products,
+      cart,
+    );
+
+    const order = await this.saveOrder(createOrderDTO, userId, payment);
+
+    await this.createOrderProductUsingCart(cart, order.id, products);
+    await this.cartService.clearCart(userId);
+
+    return order;
+  }
+
+  async findOrdersByUserId(
+    userId?: number,
+    orderId?: number,
+  ): Promise<OrderEntity[]> {
+    const orders = await this.orderRepository.find({
+      where: {
+        userId,
+        id: orderId,
+      },
+      relations: {
+        address: {
+          city: {
+            state: true,
+          },
+        },
+        ordersProduct: {
+          product: true,
+        },
+        payment: {
+          paymentStatus: true,
+        },
+        user: !!orderId,
+      },
+    });
+
+    if (!orders || orders.length === 0) {
+      throw new NotFoundException('Orders not found');
+    }
+
+    return orders;
+  }
+
+  async findAllOrders(): Promise<OrderEntity[]> {
+    const orders = await this.orderRepository.find({
+      relations: {
+        user: true,
+      },
+    });
+
+    if (!orders || orders.length === 0) {
+      throw new NotFoundException('Orders not found');
+    }
+
+    const ordersProduct =
+      await this.orderProductService.findAmountProductsByOrderId(
+        orders.map(order => order.id),
+      );
+
+    return orders.map(order => {
+      const orderProduct = ordersProduct.find(
+        currentOrder => currentOrder.order_id === order.id,
+      );
+
+      if (orderProduct) {
+        return {
+          ...order,
+          amountProducts: Number(orderProduct.total),
+        };
+      }
+      return order;
+    });
   }
 }
