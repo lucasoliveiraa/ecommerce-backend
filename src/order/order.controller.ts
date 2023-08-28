8e9b4aa606ec0b220aa8ff624506687e68ff4dc6
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-28 15:43:38 -0300

    ✨ feat: create findOrdersByUserId

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDTO } from './dto/create-order.dto';
-import { UserId } from 'src/decorators/user-id.decorator';
+import { UserId } from '../decorators/user-id.decorator';
 import { OrderEntity } from './entities/order.entity';
+import { Response } from 'express';
 
 @Controller('order')
 export class OrderController {
@@ -12,7 +13,24 @@ export class OrderController {
   async createOrder(
     @Body() createOrderDTO: CreateOrderDTO,
     @UserId() userId: number,
-  ): Promise<OrderEntity> {
+  ) {
     return this.orderService.createOrder(createOrderDTO, userId);
+  }
+
+  @Get()
+  async findOrdersByUserId(
+    @UserId() userId: number,
+    @Res({ passthrough: true }) res?: Response,
+  ): Promise<OrderEntity[]> {
+    const orders = await this.orderService
+      .findOrdersByUserId(userId)
+      .catch(() => undefined);
+
+    if (orders) {
+      return orders;
+    }
+
+    res.status(204).send();
+    return;
   }
 }
