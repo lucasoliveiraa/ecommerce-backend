c5201215eefd49d95d4700614de2e37b342670aa
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-29 16:56:45 -0300

    ✨ feat: create edit and delete categories

--- src/category/dto/return-category.dto.ts
+++ src/category/dto/return-category.dto.ts
@@ -1,18 +1,18 @@
-// import { ReturnProduct } from '../../product/dto/return-product.dto';
+import { ReturnProduct } from 'src/product/dto/return-product.dto';
 import { CategoryEntity } from '../entities/category.entity';
 
 export class ReturnCategory {
   id: number;
   name: string;
   amountProducts?: number;
-  // products?: ReturnProduct[];
+  products?: ReturnProduct[];
 
   constructor(categoryEntity: CategoryEntity, amountProducts?: number) {
     this.id = categoryEntity.id;
     this.name = categoryEntity.name;
     this.amountProducts = amountProducts;
-    // this.products = categoryEntity.products
-    //   ? categoryEntity.products.map(product => new ReturnProduct(product))
-    //   : undefined;
+    this.products = categoryEntity.products
+      ? categoryEntity.products.map(product => new ReturnProduct(product))
+      : undefined;
   }
 }
