ef608701fe9fcae86615b27c08a5acce51d84e80
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-29 16:59:42 -0300

    ✨ feat: create find orders

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDTO } from './dto/create-order.dto';
 import { UserId } from '../decorators/user-id.decorator';
 import { OrderEntity } from './entities/order.entity';
 import { Response } from 'express';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
+import { ReturnOrderDTO } from './dto/return-order.dto';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
@@ -32,5 +36,23 @@ export class OrderController {
 
     res.status(204).send();
     return;
+  }
+
+  @Roles(UserType.Admin, UserType.Root)
+  @Get('/all')
+  async findAllOrders(): Promise<ReturnOrderDTO[]> {
+    return (await this.orderService.findAllOrders()).map(
+      order => new ReturnOrderDTO(order),
+    );
+  }
+
+  @Roles(UserType.Admin, UserType.Root)
+  @Get('/:orderId')
+  async findOrderById(
+    @Param('orderId') orderId: number,
+  ): Promise<ReturnOrderDTO> {
+    return new ReturnOrderDTO(
+      (await this.orderService.findOrdersByUserId(undefined, orderId))[0],
+    );
   }
 }
