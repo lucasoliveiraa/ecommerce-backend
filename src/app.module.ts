cb12e872ffd5672cf9ec6f78a10a31996c8ec8e9
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-08-30 17:24:12 -0300

    ✨ feat: create integration api correios

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { PaymentStatusModule } from './payment-status/payment-status.module';
 import { PaymentModule } from './payment/payment.module';
 import { OrderModule } from './order/order.module';
 import { OrderProductModule } from './order-product/order-product.module';
+import { CorreiosModule } from './correios/correios.module';
 
 @Module({
   imports: [
@@ -53,6 +54,7 @@ import { OrderProductModule } from './order-product/order-product.module';
     PaymentModule,
     OrderModule,
     OrderProductModule,
+    CorreiosModule,
   ],
   controllers: [AppController],
   providers: [
